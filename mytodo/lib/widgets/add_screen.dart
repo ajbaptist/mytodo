f552107e92762225433d7df5879cf1950936b460
JohnBaptist <ajbaptist18@gmail.com>
2021-04-17 00:35:01 -0700

    modiied

--- mytodo/lib/widgets/add_screen.dart
+++ mytodo/lib/widgets/add_screen.dart
@@ -8,6 +8,30 @@ class AddTask extends StatefulWidget {
 class _AddTaskState extends State<AddTask> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 800,
+      color: Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                'Add Task',
+                style: TextStyle(color: Colors.lightBlueAccent, fontSize: 30),
+              ),
+              TextField(
+                decoration: InputDecoration(hintText: 'TYPE HERE'),
+              ),
+              TextButton(onPressed: null, child: Text('ADD'))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- mytodo/lib/widgets/task_tile.dart
+++ mytodo/lib/widgets/task_tile.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    Key key,
-  }) : super(key: key);
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool mycheck = false;
+  void callback(bool newval) {
+    setState(() {
+      mycheck = newval;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Text('my data is here'),
-        trailing: Checkbox(
-          value: false,
-          onChanged: null,
+        title: Text(
+          'my data is here',
+          style: TextStyle(
+              decoration: mycheck ? TextDecoration.lineThrough : null),
+        ),
+        trailing: CheckBox(
+          mycheckbox: mycheck,
+          mytoggle: callback,
         ));
+  }
+}
+
+class CheckBox extends StatelessWidget {
+  final mycheckbox;
+  final Function mytoggle;
+  CheckBox({this.mycheckbox, this.mytoggle});
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: mycheckbox,
+        onChanged: mytoggle);
   }
 }
